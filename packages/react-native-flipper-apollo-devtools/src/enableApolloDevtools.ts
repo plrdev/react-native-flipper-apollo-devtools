e15c492fd0ca6d1e1c22f2e64c19bef1279bddfb
Juha Linnanen <juha.linnanen@gofore.com>
2020-09-27 18:16:47 +0300

    Fix ts build

--- packages/react-native-flipper-apollo-devtools/src/enableApolloDevtools.ts
+++ packages/react-native-flipper-apollo-devtools/src/enableApolloDevtools.ts
@@ -1,17 +1,29 @@
 import { addPlugin, Flipper } from 'react-native-flipper'
 import { ApolloClient, NormalizedCache, NormalizedCacheObject } from '@apollo/client'
 
-type Client = ApolloClient<NormalizedCache | NormalizedCacheObject>
+type ApolloDevtoolCallbackParams = {
+  state: any
+  dataWithOptimisticResults: any
+}
+
+type ApolloDevClient<T> = Omit<ApolloClient<T>, '__actionHookForDevTools'> & {
+  __actionHookForDevTools: (params: (arg0: ApolloDevtoolCallbackParams) => any) => void
+}
 
 let currentConnection: Flipper.FlipperConnection | null = null
-let client: Client | null = null
-export const enableFlipperApolloDevtools = (newClient: Client) => {
+let client: ApolloDevClient<NormalizedCache | NormalizedCacheObject> | null = null
+export const enableFlipperApolloDevtools = (
+  newClient: ApolloDevClient<NormalizedCache | NormalizedCacheObject>
+) => {
   if (client !== newClient) {
     client = newClient
 
     let counter = 0
     let enqueued = null
-    const logger = ({ state: { queries, mutations }, dataWithOptimisticResults: inspector }) => {
+    const logger = ({
+      state: { queries, mutations },
+      dataWithOptimisticResults: inspector,
+    }: ApolloDevtoolCallbackParams) => {
       counter++
       enqueued = {
         counter,
